7e982886c509019a8feb761b9db134d02ccd783e
LorenzoASC23 <139983093+LorenzoASC23@users.noreply.github.com>
2026-03-03 20:19:50 +0000

    done

--- index.js
+++ index.js
@@ -25,22 +25,68 @@ app.use((req, res, next) => {
 
 
 // 5) Paste in your data array of objects from the planning documents here. 
+const data = 
+{
+    "appName": "FlavorShare",
+  "users": [
+    {
+      "id": 1,
+      "username": "chefLorenzo",
+      "email": "lorenzo@example.com"
+    }
+  ],
+  "recipes": [
+    {
+      "id": 101,
+      "title": "Classic Margherita Pizza",
+      "ingredients": [
+        "Pizza dough",
+        "Tomato sauce",
+        "Fresh mozzarella",
+        "Basil leaves",
+        "Olive oil"
+      ],
+      "instructions": "Spread sauce on dough, add cheese, bake at 475°F for 12 minutes, top with basil and olive oil.",
+      "authorId": 1
+    }
+  ],
+  "categories": [
+    "Italian",
+    "Dessert",
+    "Vegan",
+    "Quick Meals",
+    "Gluten-Free"
+  ],
+  "reviews": [
+    {
+      "id": 5001,
+      "recipeId": 101,
+      "userId": 1,
+      "rating": 5,
+      "comment": "Simple and delicious!"
+    }
+  ]
+}
 
 
 // 6) Add static file middleware here
-
+app.use(express.static(__dirname + '/public'))
 
 // 7) Set the view engine to ejs here
-
+app.set('view engine', 'ejs')
 
 // 8) Create a route handler to path / and send your index.html 
 // run your server with node index.js to test it
-
+app.get("/" , (req, res) => {
+  res.render('index.html', data)
+})
 
 
 // 9) Create a route handler to your overview route from your planning document
 // Pass in your data array
-
+app.get("/overview" , (req, res) => {
+res.render('recipe.ejs', data)
+})
 
 
 
